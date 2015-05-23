f423210c28de6494bd8a0cd3213877f17ac77c59
cory@lamle.org <cory@lamle.org@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 22:33:32 +0000

    version fix
    git-svn-id: https://plugins.svn.wordpress.org/duplicator/tags/0.5.20@1166436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- define.php
+++ define.php
@@ -1,7 +1,7 @@
 <?php
 //Prevent directly browsing to the file
 if (function_exists('plugin_dir_url')) {
-    define('DUPLICATOR_VERSION',        '0.5.19');
+    define('DUPLICATOR_VERSION',        '0.5.20');
     define("DUPLICATOR_HOMEPAGE",       "http://lifeinthegrid.com/labs/duplicator");
     define("DUPLICATOR_GIVELINK",       "http://lifeinthegrid.com/partner");
     define("DUPLICATOR_HELPLINK",       "http://lifeinthegrid.com/duplicator-docs");
