d96146b5edf8cb2840b9977d54d49316c217a533
cory@lamle.org <cory@lamle.org@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-01-26 16:34:38 +0000

    localize fix
    git-svn-id: https://plugins.svn.wordpress.org/duplicator/trunk@1336455 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/packages/details/transfer.php
+++ views/packages/details/transfer.php
@@ -4,11 +4,9 @@
 	div.transfer-hdr { border-bottom: 0px solid #dfdfdf; margin: -15px 0 0 0}
 </style>
 
-
 <div class="transfer-panel">
-
 	<div class="transfer-hdr">
-		<h2><i class="fa fa-arrow-circle-right"></i> <?php DUP_PRO_U::_e('Manual Transfer'); ?></h2>
+		<h2><i class="fa fa-arrow-circle-right"></i> <?php DUP_Util::_e('Manual Transfer'); ?></h2>
 	</div>
 	<br/>
 	
@@ -21,19 +19,9 @@
 			DUP_Util::_e('Simply choose your destination and hit the transfer button and your done.');
 		?>
 	</div>
-	
 	<p style="text-align:center">
 		<a href="http://snapcreek.com/duplicator?free-manual-transfer" target="_blank" class="button button-primary button-large dup-check-it-btn" >
 			<?php DUP_Util::_e('Learn More') ?>
 		</a>
 	</p>
-
-
 </div>
-
-
-<script type="text/javascript">
-    jQuery(document).ready(function ($) {
-
-    });
-</script>
