c646c87fb4c695302766d2c9aa0ebe150935ea1e
cory@lamle.org <cory@lamle.org@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-12-28 19:30:36 +0000

    - version date
    git-svn-id: https://plugins.svn.wordpress.org/duplicator/trunk@2003089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- define.php
+++ define.php
@@ -3,7 +3,7 @@
 if (function_exists('plugin_dir_url')) 
 {		
     define('DUPLICATOR_VERSION',        '1.3.2');
-	define('DUPLICATOR_VERSION_BUILD',  '2018-12-24_10:08');
+	define('DUPLICATOR_VERSION_BUILD',  '2018-12-28_12:33');
     define('DUPLICATOR_PLUGIN_URL',     plugin_dir_url(__FILE__));
 	define('DUPLICATOR_SITE_URL',		get_site_url());
 	
