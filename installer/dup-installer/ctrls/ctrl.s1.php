6b2943f529b2a451443734fcf0e50790bf3efe92
cory@lamle.org <cory@lamle.org@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2022-08-27 19:24:27 +0000

    removing legacy code, version update
    git-svn-id: https://plugins.svn.wordpress.org/duplicator/trunk@2776550 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- installer/dup-installer/ctrls/ctrl.s1.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('ABSPATH') || defined('DUPXABSPATH') || exit;
-
-require_once($GLOBALS['DUPX_INIT'].'/ctrls/classes/class.ctrl.extraction.php');
-
-$data = DUP_LITE_Extraction::getInstance()->runExtraction();
-die(DupLiteSnapJsonU::wp_json_encode($data));
--- lib/dup_archive/classes/class.duparchive.loggerbase.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('ABSPATH') || defined('DUPXABSPATH') || exit;
-if(!class_exists('DupArchiveLoggerBase')) {
-abstract class DupArchiveLoggerBase
-{
-    abstract public function log($s, $flush = false, $callingFunctionOverride = null);
-}
-}
